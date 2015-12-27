e9eef4e3518d4ea6fa445ae04caf9a618e821d5e
TyerTemp <tylertempdev@gmail.com>
2015-12-27 14:26:55 +0800

    fix for brey

--- lib/hdlr/utility/sina/home.py
+++ lib/hdlr/utility/sina/home.py
@@ -24,12 +24,8 @@ class HomeHandler(BaseHandler):
         app_key = self.get_argument('app-key')
         app_secret = self.get_argument('app-secret')
         self.set_app(app_key, app_secret)
-        host = self.request.host
         client = Client(api_key=app_key, api_secret=app_secret,
-                        redirect_uri=''.join(
-                            ('https://',
-                             host,
-                             '/utility/sina/callback/')))
+                        redirect_uri=self.callback_url)
 
         return self.write(json.dumps({'url': client.authorize_url}))
 
