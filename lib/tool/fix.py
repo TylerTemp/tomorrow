d440c262d036808ec4a81b902af334976fbc1840
TyerTemp <tylertempdev@gmail.com>
2016-02-03 01:22:06 +0800

    fix jolla list title

--- lib/tool/fix.py
+++ lib/tool/fix.py
@@ -7,10 +7,12 @@ import os
 sys.path.insert(0, os.path.normpath(os.path.join(__file__, '..', '..', '..')))
 from lib.tool.bashlog import stdoutlogger, DEBUG
 from lib.config.base import Config
+from lib.db.jolla import Article
 sys.path.pop(0)
 
 logger = stdoutlogger(None, DEBUG)
 config = Config()
+config.auto_clean = False
 
 
 def mkdir():
@@ -21,5 +23,13 @@ def mkdir():
             os.mkdir(path)
 
 
+def fix_jolla():
+    col = Article.collection
+    for count, each in enumerate(col.find({'source.author': 'jolla'}), 1):
+        each['source']['author'] = 'Jolla'
+        col.replace_one({'_id': each['_id']}, each)
+        logger.info('[%s] fixed, %s', count, each['slug'])
+
 if __name__ == '__main__':
     mkdir()
+    fix_jolla()
