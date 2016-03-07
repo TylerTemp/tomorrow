f2efabfd33c697fbba2fcd4d116b12d7b0487b1b
TylerTemp <tylertempdev@gmail.com>
2016-03-07 19:00:53 +0800

    jolla paser

--- lib/tool/cmd/new_trans.py
+++ lib/tool/cmd/new_trans.py
@@ -58,12 +58,93 @@ def parse_reviewjolla(url):
         if small.endswith('favicon.png'):
             continue
 
-        big = each.parent.get('href')
-        if big == 'javascript:;':
+        parent = each.parent
+        if parent.name == 'a':
+            big = parent.get('href')
+            if big == 'javascript:;':
+                big = None
+        else:
             big = None
+
         imgs.append((small, big))
 
+    result['author'] = 'Simo Ruoho'
+
     return result
+
+
+def parse_jolla(url):
+    soup = mk_soup(url)
+    # with open('/tmp/save.html', 'r', encoding='utf-8') as f:
+    #     soup = BeautifulSoup(f.read(), 'html5lib')
+    #     # f.write(soup.prettify())
+
+    result = {}
+
+    img_div = soup.find(None, {'class': 'blog-img-wrap'})
+    banner = img_div.find('img').get('src')
+    logger.debug(banner)
+    result['banner'] = banner
+
+    tags = set()
+    tags_tag = soup.find(None, {'class': 'blog-cats'})
+    for a_tag in tags_tag.find_all('a'):
+        tags.add(a_tag.text.lower().strip())
+    logger.debug(tags)
+
+    author = soup.find(None, {'class': 'author-name'}).text.strip()
+    logger.debug(author)
+    result['author'] = author
+
+    blog = soup.find(None, {'class': 'blog-wrap'})
+    title = blog.find('h1').text.strip()
+    result['title'] = title
+
+    imgs = []
+    for img in blog.find_all('img'):
+        small = img.get('src')
+        parent = img.parent
+        if parent.name == 'a':
+            big = parent.get('href')
+            if big == 'javascript:;':
+                big = None
+        else:
+            big = None
+
+        if small == big:
+            small = None
+
+        imgs.append((small, big))
+
+    result['imgs'] = imgs
+
+    result['cover'] = _find_jolla_cover(url)
+    return result
+
+
+def _find_jolla_cover(url):
+    page = 1
+    while True:
+        if page == 1:
+            url_page = 'https://blog.jolla.com/'
+        else:
+            url_page = 'https://blog.jolla.com/page/%s/' % page
+        logger.debug('looking %s', url_page)
+
+        resp = requests.get(url_page)
+        if resp.status_code >= 400:
+            logger.critical('%s not found', url_page)
+            return None
+
+        soup = mk_soup(url_page)
+        for article in soup.find_all('article'):
+            right = article.find(None, {'class': 'cont-right'})
+            a_tag = right.find('a')
+            href = a_tag.get('href')
+            if href == url:
+                return a_tag.find('img').get('src')
+
+        page += 1
 
 
 if __name__ == '__main__':
@@ -71,5 +152,5 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     logging.getLogger('docpie').setLevel(logging.CRITICAL)
 
-    url = 'http://reviewjolla.blogspot.sg/2016/03/follow-up-promise-on-sailfish-os-in.html'
-    pprint(parse_reviewjolla(url))
+    url = 'https://blog.jolla.com/road-sailfish-os-2-0/'
+    pprint(parse_jolla(url))
